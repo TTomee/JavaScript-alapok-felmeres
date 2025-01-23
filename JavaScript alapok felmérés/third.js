657846087f7f4d61ba40acc13d69a669be715fd1
Fekete Tamás <tamasft09@gmail.com>
2025-01-23 20:41:36 +0200

    third.js

--- JavaScript alapok felmérés/third.js
+++ JavaScript alapok felmérés/third.js
@@ -2,7 +2,7 @@ let kezdet = Number(prompt("Az intervallum kezdete:"));
 let veg = Number(prompt("Az intervallum vége:"));
 
 for (let i = kezdet; i <= veg; i++) {
-    if (i % 2 === 0) {
-        document.write(`<br>${i}`);
+    if (i % 2 == 0) {
+        document.write(i);
     }
 }
